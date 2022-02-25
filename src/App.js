ec7560e10dad6ff4785c7dc94164460feb7fdbdd
Savita_N <62276914+Savita01@users.noreply.github.com>
2022-02-25 19:43:59 +0530

    code refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { Container, Grid, Segment, Statistic, Icon } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import DisplayBalance from "./Componets/DisplayBalance";
+import EntryLine from "./Componets/EntryLine";
 import MainHeader from "./Componets/MainHeader";
 import NewEntryForm from "./Componets/NewEntryForm";
+import DisplayBalances from "./Componets/DisplayBalances";
 
 function App() {
   return (
@@ -13,69 +15,11 @@ function App() {
         size="small"
         color="black"
       />
-
-      <Segment textAlign="center">
-        <Grid columns={2} divided>
-          <Grid.Row>
-            <Grid.Column>
-              <DisplayBalance title="income" value="1,045.50" color="green" />
-            </Grid.Column>
-            <Grid.Column>
-              <DisplayBalance title="Expence" value="653.50" color="red" />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <DisplayBalances />
       <MainHeader title="History" type="h3" />
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">
-              something
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="right">
-              $10,00
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color="green">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">
-              something2
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="right">
-              $20,00
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">
-              something3
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="right">
-              $30,00
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <MainHeader title=" Add new transaction" type="h3" />
+      <EntryLine description="something" value="12.00" />
+      <EntryLine description="something 2" value="43.00" isExpenses />
+      <MainHeader title="Add new transaction" type="h3" />
       <NewEntryForm />
     </Container>
   );
---
+++ src/Componets/DisplayBalances.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Segment, Grid } from "semantic-ui-react";
+import DisplayBalance from "./DisplayBalance";
+function DisplayBalances() {
+  return (
+    <Segment textAlign="center">
+      <Grid columns={2} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <DisplayBalance title="income" value="1,045.50" color="green" />
+          </Grid.Column>
+          <Grid.Column>
+            <DisplayBalance title="Expence" value="653.50" color="red" />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  );
+}
+
+export default DisplayBalances;
